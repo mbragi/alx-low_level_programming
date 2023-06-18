c0c09b965d8ac3664f1787b3884580fada3027ba
Gar Manji Michael <garmanjimichael@gmail.com>
2023-06-18 02:41:32 -0700

    fix betty documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the string "Programming is like
+ *              building a multilingual puzzle" using the puts function.
+ *              It then returns 0 to indicate successful termination.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
