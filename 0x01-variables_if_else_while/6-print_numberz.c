61258aee2462e07f721f2c2565dc36018c9b264e
Gar Manji Michael <garmanjimichael@gmail.com>
2023-06-25 02:07:30 -0700

    Vsriables and if else commit initial

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints all single-digit numbers of base 10
+ *              starting from 0, followed by a new line. It uses the
+ *              `putchar` function to print each digit.
+ *
+ * Return: Always 0 (indicating success)
+ */
+int main(void)
+{
+    putchar('0' + 0);
+    putchar('0' + 1);
+    putchar('0' + 2);
+    putchar('0' + 3);
+    putchar('0' + 4);
+    putchar('0' + 5);
+    putchar('0' + 6);
+    putchar('0' + 7);
+    putchar('0' + 8);
+    putchar('0' + 9);
+    putchar('\n');
+
+    return 0;
+}
+
