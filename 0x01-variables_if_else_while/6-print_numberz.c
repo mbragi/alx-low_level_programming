6879cd2499459bf4531d16ee6699700b38a322a3
Gar Manji Michael <garmanjimichael@gmail.com>
2023-06-25 02:10:24 -0700

    Vsriables and if else commit initial

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,26 +3,29 @@
 /**
  * main - Entry point of the program
  *
- * Description: This function prints all single-digit numbers of base 10
- *              starting from 0, followed by a new line. It uses the
- *              `putchar` function to print each digit.
+ * Description: This program prints all single-digit numbers of base 10
+ *              starting from 0, followed by a new line. It uses the putchar
+ *              function to print the digits and adheres to the Betty coding
+ *              style and documentation style.
  *
  * Return: Always 0 (indicating success)
  */
 int main(void)
 {
-    putchar('0' + 0);
-    putchar('0' + 1);
-    putchar('0' + 2);
-    putchar('0' + 3);
-    putchar('0' + 4);
-    putchar('0' + 5);
-    putchar('0' + 6);
-    putchar('0' + 7);
-    putchar('0' + 8);
-    putchar('0' + 9);
-    putchar('\n');
+	putchar('0');
+	putchar('\n');
 
-    return 0;
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+	putchar('\n');
+
+	return (0);
 }
 
